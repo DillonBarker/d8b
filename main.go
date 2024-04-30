f8ef8be66cecddfdc5c3edd1fcb35b0a81bbd3d4
dillon.barker <barker.dill98@gmail.com>
2024-04-30 15:09:28 +0100

    working simple implementation using tview

--- main.go
+++ main.go
@@ -2,46 +2,52 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"strings"
 
+	"github.com/DillonBarker/d8b/src/db"
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
-type TableData struct {
-	tview.TableContentReadOnly
-}
+func getTableData() *tview.Table {
+	query := "SELECT * from organisation"
+	table := tview.NewTable().
+		SetBorders(false).
+		SetSelectable(true, false)
 
-func (d *TableData) GetCell(row, column int) *tview.TableCell {
-	letters := [...]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'A' + byte(row%26)} // log(math.MaxInt64) / log(26) ~= 14
-	start := len(letters) - 1
-	row /= 26
-	for row > 0 {
-		start--
-		row--
-		letters[start] = 'A' + byte(row%26)
-		row /= 26
+	rowData, headers, err := db.ExecuteQuery(query)
+
+	if err != nil || headers == nil {
+		panic(err)
 	}
-	return tview.NewTableCell(fmt.Sprintf("[red]%s[green]%d", letters[start:], column))
-}
 
-func (d *TableData) GetRowCount() int {
-	return math.MaxInt64
-}
+	for headerIndex, header := range headers {
+		table.SetCell(0, headerIndex,
+			tview.NewTableCell(strings.ToUpper(header)).
+				SetTextColor(tcell.ColorWhite).
+				SetAlign(tview.AlignLeft))
+	}
 
-func (d *TableData) GetColumnCount() int {
-	return math.MaxInt64
+	for rowIndex, row := range rowData {
+		for itemIndex, item := range row {
+			color := tcell.ColorWhite
+
+			table.SetCell(rowIndex+1, itemIndex,
+				tview.NewTableCell(item).
+					SetTextColor(color).
+					SetAlign(tview.AlignLeft))
+		}
+	}
+
+	table.
+		SetTitle(fmt.Sprintf(" %s  [%d] ", query, len(rowData))).
+		SetBorder(true)
+	return table
 }
 
 func makeTable() *tview.Table {
-	data := &TableData{}
-	table := tview.NewTable().
-		SetBorders(false).
-		SetSelectable(true, false).
-		SetContent(data)
+	table := getTableData()
 
-	table.SetBorder(false)
-
-	table.SetTitle(" table x y z ")
 	return table
 }
 
